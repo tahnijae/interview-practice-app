41ec6e717e1bcd12e7cdbebe91eda0cab780cf5e
Tahni Hresko <124373508+tahnijae@users.noreply.github.com>
2023-03-05 17:17:40 -0500

    initial commit

---
+++ InterviewPracticeProject/CaptureForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterviewPracticeProject
+{
+    public partial class CaptureForm : Form
+    {
+        public CaptureForm()
+        {
+            InitializeComponent();
+        }
+
+        private void cbx_showQuestion_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbx_displayQuestion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbtn_showInterviewer_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbtn_showSelf_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_skip_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_repeatQuestion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_stopRecording_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btn_backToMainMenu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# interview-practice-app
+A Windows Forms app in C# for recording responses to randomly generated interview questions
